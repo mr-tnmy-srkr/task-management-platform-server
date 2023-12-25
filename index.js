39bf9974411a824ca6b04d64c4dd9881f07c6e5f
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-25 23:39:00 +0530

    get,post,put,patch,delete api end point created

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 require("dotenv").config();
 
-
 const client = new MongoClient(process.env.DB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,17 +23,95 @@ const tasksCollection = client.db("taskManagementDB").collection("tasks");
 
 async function run() {
   try {
-     // Connect the client to the server	(optional starting in v4.7)
-  client.connect();
-  app.post("/tasks", async (req, res) => {
-    const tasks = req.body;
-    const result = await tasksCollection.insertOne(tasks);
-    res.send(result);
-  });
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+    app.post("/add-tasks", async (req, res) => {
+      try {
+        const tasks = req.body;
+        const result = await tasksCollection.insertOne(tasks);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/get-tasks/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+        const query = { email: email };
+        const result = await tasksCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+    app.get("/get-task/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await tasksCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+    app.put("/update-task/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const updatedTasks = req.body;
+        const query = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            ...updatedTasks
+          },
+        };
+        const result = await tasksCollection.updateOne(
+          query,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+    app.delete("/delete-tasks/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await tasksCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+    app.patch("/task-type-update/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const newType = req.body;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            type: newType.type,
+          },
+        };
+        const result = await tasksCollection.updateOne(query, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     try {
-    client.db("admin").command({ ping: 1 });
+      client.db("admin").command({ ping: 1 });
       console.log(
         "Pinged your deployment. You successfully connected to MongoDB!"
       );
